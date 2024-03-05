2921ea1c63d70a3958ee3f3e6ff27475ec984f62
william victor <vickenconcept@gmail.com>
2024-03-05 15:35:46 -0800

    update made

--- src/Area/Rectangle.php
+++ src/Area/Rectangle.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Area;
+namespace Area\Shapes;
 
 class Rectangle
 {
     public static function area($length, $width)
     {
         return $length * $width;
-        
+
     }
 }
